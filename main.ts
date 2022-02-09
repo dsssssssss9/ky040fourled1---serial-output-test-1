cbd73a3688abb383c294cdd64876961b76625294
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2022-02-09 19:17:49 +0000

    set xcord bounding to hard coded values equal to number of LED

--- main.ts
+++ main.ts
@@ -8,6 +8,9 @@ KY040.OnPinPressed(DigitalPin.P0, function () {
 })
 KY040.onTurned(direction.clockwise, function () {
     Xcord = Xcord + 1
+    if (Xcord > 4) {
+        Xcord = 4
+    }
     serial.writeValue("Xcord", Xcord)
     if (Xcord == 1) {
         pins.digitalWritePin(DigitalPin.P2, 1)
@@ -32,7 +35,37 @@ KY040.onTurned(direction.clockwise, function () {
     }
 })
 KY040.onTurned(direction.counterclockwise, function () {
-	
+    Xcord = Xcord - 1
+    if (Xcord < 1) {
+        Xcord = 0
+    }
+    serial.writeValue("Xcord", Xcord)
+    if (Xcord == 1) {
+        pins.digitalWritePin(DigitalPin.P2, 1)
+        pins.digitalWritePin(DigitalPin.P3, 0)
+        pins.digitalWritePin(DigitalPin.P4, 0)
+        pins.digitalWritePin(DigitalPin.P5, 0)
+    } else if (Xcord == 2) {
+        pins.digitalWritePin(DigitalPin.P3, 1)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        pins.digitalWritePin(DigitalPin.P4, 0)
+        pins.digitalWritePin(DigitalPin.P5, 0)
+    } else if (Xcord == 3) {
+        pins.digitalWritePin(DigitalPin.P4, 1)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        pins.digitalWritePin(DigitalPin.P3, 0)
+        pins.digitalWritePin(DigitalPin.P5, 0)
+    } else if (Xcord == 4) {
+        pins.digitalWritePin(DigitalPin.P5, 1)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        pins.digitalWritePin(DigitalPin.P3, 0)
+        pins.digitalWritePin(DigitalPin.P4, 0)
+    } else if (Xcord <= 0) {
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        pins.digitalWritePin(DigitalPin.P3, 0)
+        pins.digitalWritePin(DigitalPin.P4, 0)
+        pins.digitalWritePin(DigitalPin.P5, 0)
+    }
 })
 let Xcord = 0
 led.enable(false)
